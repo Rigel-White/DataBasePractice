8b8ed5c103aa3620ed7b33bef3402631abde1ef7
Kvirel <aleksej_yakovlev_2005@bk.ru>
2024-11-03 14:07:34 +0900

    Добавьте файлы проекта.

---
+++ DataBasePractice/DataBase.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+
+namespace DataBasePractice
+{
+    class DataBase
+    {
+        SqlConnection sqlConnection =new SqlConnection(@"Data Source = DESKTOP-M9E9S8K;Initial Catalog = test_db;Integred Security = True");
+        public void openConnection()
+        {
+            if (sqlConnection.State == System.Data.ConnectionState.Closed) { sqlConnection.Open(); }
+        }
+        public void closeConnection()
+        {
+            if (sqlConnection.State == System.Data.ConnectionState.Open) { sqlConnection.Close(); }
+        }
+        public SqlConnection getConnection()
+        {
+            return sqlConnection;
+        }
+    }
+}
