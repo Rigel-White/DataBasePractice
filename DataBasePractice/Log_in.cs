363d4cc5820914a66e68aef206981b67de9934a8
Kvirel <aleksej_yakovlev_2005@bk.ru>
2024-11-10 15:41:16 +0900

    DatabasePractice2

---
+++ DataBasePractice/Log_in.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace DataBasePractice
+{
+    public partial class Log_in : Form
+    {
+        DataBase database = new DataBase();
+        public Log_in()
+        {
+            InitializeComponent();
+        }
+        private void Log_in_Load(object sender, EventArgs e)
+        {
+            textBox2.PasswordChar = '*';
+            textBox1.MaxLength = 50;
+            textBox2.MaxLength = 50;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            var loginUser = textBox1.Text;
+            var passUser = textBox2.Text;
+
+            SqlDataAdapter adapter = new SqlDataAdapter();
+            DataTable table = new DataTable();
+
+            string querystring = $"select id_user, login_user, password_user from register where login_user = '{loginUser}' and password_user = '{passUser}'";
+
+            SqlCommand command = new SqlCommand(querystring, database.getConnection());
+
+            adapter.SelectCommand = command;
+            adapter.Fill(table);
+
+            if (table.Rows.Count == 1)
+            {
+                MessageBox.Show("Вы успешно вошли!", "Успешно", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                Form1 frm = new Form1();
+                this.Hide();
+                frm.ShowDialog();
+                this.Show();
+            }
+            else
+            {
+                MessageBox.Show("Такого аккаунта не существует!", "Аккаунта не существует!", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+        }
+
+        private void linkLabel1_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            Sign_up sign_up = new Sign_up();
+            sign_up.Show();
+            this.Hide();
+        }
+    }
+}
